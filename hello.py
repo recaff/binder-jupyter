3aa4aa6158868b5a77a642f662bc4117cf282466
recaff <recaff@users.noreply.github.com>
2022-12-25 23:52:21 +0800

    Update hello.py

--- hello.py
+++ hello.py
@@ -3,5 +3,3 @@ import sys
 major_version = sys.version_info.major
 minor_version = sys.version_info.minor
 print("Hello from Binder Python {}.{}!".format(major_version, minor_version))
-
-liubin testing
